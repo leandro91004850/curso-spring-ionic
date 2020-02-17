b1329553e96ad3c8ecb66b7a7369dc5a07d277f1
leandro91004850 <leandroleekrap@gmail.com>
2020-02-17 20:26:17 -0300

    navegacao

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,8 +9,15 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) { //injeção de independencia
 
   }
+// navegação da pagina home para categoria
+
+  login(){
+     // this.navCtrl.push('CategoriaPage'); // empilhar uma pagina sobre a outra com push
+     this.navCtrl.setRoot('CategoriaPage');
+  
+    }
 
 }
