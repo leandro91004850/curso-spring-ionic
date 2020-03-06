de2d4d1d0e4d244e3f7819c915a74fb73cfce669
leandro91004850 <leandroleekrap@gmail.com>
2020-03-06 11:33:20 -0300

    Terminando as funcionalidades do carrinho

--- src/services/domain/cart.service.ts
+++ src/services/domain/cart.service.ts
@@ -34,4 +34,52 @@ export class CartService {
         this.storage.setCart(cart);
         return cart;
     }
+
+    // remover o item do carrinho
+    removeProduto(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if (position != -1) {
+            cart.items.splice(position, 1); // se o valor encontrado for diferente de 1 sera removido do carrinho
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    increaseQuantity(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if (position != -1) {
+            cart.items[position].quantidade++; // implementar a quantidade do produto
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    // metodo responsável pela remoção do itens do carrinho
+    decreaseQuantity(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if (position != -1) {
+            cart.items[position].quantidade--; 
+            if(cart.items[position].quantidade <1){
+                cart = this.removeProduto(produto);
+            }
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    // metodo responsavel pela soma do itens adicionados no carrinho
+    total(): number{
+        let cart = this.getCart();
+        let sum = 0;
+        for(var i= 0; i < cart.items.length; i++){
+            sum += cart.items[i].produto.preco * cart.items[i].quantidade;
+
+        }
+        return sum;
+    }
+
+
 }
