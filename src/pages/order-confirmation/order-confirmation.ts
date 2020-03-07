60e53c6774f502d32e1e8dd81b42cba7c02e6965
leandro91004850 <leandroleekrap@gmail.com>
2020-03-06 21:18:29 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { ClienteDTO } from '../../models/cliente.dto';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 
 @IonicPage()
@@ -25,7 +26,8 @@ export class OrderConfirmationPage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public PedidoService: PedidoService) {
 
     this.pedido = this.navParams.get('pedido');
   }
@@ -52,4 +54,20 @@ export class OrderConfirmationPage {
       return this.cartService.total();
     }
 
+    back(){
+      this.navCtrl.setRoot('cartPage');
+    }
+
+    checkout(){
+      this.PedidoService.insert(this.pedido)
+      .subscribe(response =>{
+        this.cartService.createOrClearCart();
+        console.log(response.headers.get('location'));
+      },
+      error => {
+         if(error.status == 403){
+           this.navCtrl.setRoot('HomePage');
+         }
+      });
+    }
 }
