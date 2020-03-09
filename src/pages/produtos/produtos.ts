14960bee81978a0a8e4cfa7471d203a4c165eed1
leandro91004850 <leandroleekrap@gmail.com>
2020-03-09 13:52:36 -0300

    Reflesher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -21,6 +21,10 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData(){
     let categoria_id = this.navParams.get('cat');
     let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
@@ -32,7 +36,6 @@ export class ProdutosPage {
       error => {
         loader.dismiss();
       });
-
   }
 
   loadImageUrls() {
@@ -60,6 +63,13 @@ export class ProdutosPage {
     return loader;
   }
 
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
+  }
 
 
 }
